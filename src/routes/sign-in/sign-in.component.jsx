e0d19924d489f0c825c3ee3f4bfbbc6e59b1d4cf
Andre Rodrigues <andre.rodrigues@giffits.de>
2022-10-13 14:56:34 +0100

    created signup component, form input component, button component

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,6 +1,9 @@
 import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
 
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
 const SignIn = () => {
+
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
@@ -12,6 +15,7 @@ const SignIn = () => {
             <button onClick={logGoogleUser}>
                 Sign in with google popup
             </button>
+            <SignUpForm />
         </div>
     )
 }
