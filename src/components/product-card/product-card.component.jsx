b0bf9b4d0a8193b28d2b3bdcd08b393896202f67
Andre Rodrigues <andre.rodrigues@giffits.de>
2022-10-20 20:27:52 +0100

    changed everything to use styled components

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -4,7 +4,7 @@ import { CartContext } from '../../contexts/cart.context';
 
 import Button from '../button/button.component';
 
-import './product-card.styles.scss';
+import { ProductCartContainer, Footer, Name, Price } from './product-card.styles';
 
 
 const ProductCard = ({product}) => {
@@ -14,14 +14,14 @@ const ProductCard = ({product}) => {
     const addProductToCart = () => addItemToCart(product)
 
     return(
-        <div className='product-card-container'>
+        <ProductCartContainer>
             <img src={imageUrl} alt={`${name}`} />
-            <div className='footer'>
-                <span className='name'>{name}</span>
-                <span className='price'>{price}</span>
-            </div>
+            <Footer>
+                <Name>{name}</Name>
+                <Price>{price}</Price>
+            </Footer>
             <Button buttonType='inverted' onClick={addProductToCart}>Add to card</Button>
-        </div>
+        </ProductCartContainer>
     )
 }
 
--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -5,7 +5,7 @@ import Button from '../button/button.component';
 
 import {signInWithGooglePopup, signInAuthUserWithEmailAndPassword } from '../../utils/firebase/firebase.utils'
 
-import './sign-in-form-styles.scss';
+import { SignUpContainer, ButtonsContainer } from './sign-in-form.styles';
 
 const defaultFormFields = {
     email: '',
@@ -41,18 +41,18 @@ const SignInForm = () => {
     }
 
     return (
-        <div className='sign-up-container'>
+        <SignUpContainer>
             <h2>Already have an account?</h2>
             <span>Sign in with your email and password</span>
             <form onSubmit={handleSubmit}>
                 <FormInput label="Email" type="email" required onChange={handleChange} name="email" value={email} />
                 <FormInput label="Password" type="password" required onChange={handleChange} name="password" value={password} />
-                <div className='buttons-container'>
+                <ButtonsContainer>
                     <Button type="submit">Sign In</Button>
                     <Button type="button" onClick={signInWithGoogle} buttonType='google'>Google Sign In</Button>
-                </div>
+                </ButtonsContainer>
             </form>
-        </div>
+        </SignUpContainer>
     )
 }
 
